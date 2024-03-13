7e771b11a892f82b68f6e547c7b02129f66a97f9
Pierre-Henry Soria <pierre@ph7.me>
2024-03-14 08:59:56 +1100

    minor improvements in code/UI

--- Succeed AI/WindowManager.swift
+++ Succeed AI/WindowManager.swift
@@ -1,5 +1,4 @@
 import SwiftUI
-import SwiftUIX
 
 class WindowManager {
     static let shared = WindowManager()
@@ -7,25 +6,18 @@ class WindowManager {
 
     func openSettings() {
         DispatchQueue.main.async {
-            self.createAndShowSettingsWindow()
+            if self.settingsWindow == nil {
+                let settingsView = UserSettingsView()
+                self.settingsWindow = NSWindow(
+                    contentRect: NSRect(x: 0, y: 0, width: 300, height: 200),
+                    styleMask: [.titled, .closable],
+                    backing: .buffered, defer: false
+                )
+                self.settingsWindow?.center()
+                self.settingsWindow?.contentView = NSHostingView(rootView: settingsView)
+            }
+            self.settingsWindow?.makeKeyAndOrderFront(nil)
+            NSApp.activate(ignoringOtherApps: true)
         }
-    }
-
-    private func createAndShowSettingsWindow() {
-        if self.settingsWindow == nil {
-            let settingsView = UserSettingsView()  // Your SwiftUI settings view
-            let window = NSWindow(
-                contentRect: NSRect(x: 0, y: 0, width: 200, height: 200),
-                styleMask: [.titled, .closable],
-                backing: .buffered, defer: false
-            )
-            window.center()
-            window.setFrameAutosaveName("Settings")
-            window.contentView = NSHostingView(rootView: settingsView)
-
-            self.settingsWindow = window
-        }
-        self.settingsWindow?.makeKeyAndOrderFront(nil)
-        NSApp.activate(ignoringOtherApps: true)
     }
 }
